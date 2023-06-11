604d8ff68c3acb33c68bb6516b5742d1f8fc9451
lparamedicl <kondaurovr1@gmail.com>
2023-06-11 20:25:29 +0500

    xz

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,7 @@
 </script>
 
 <template>
-
+    <form @:submit.prevent="addUser">{{ newUser }}</form>
+<input type="text" name="username" v-model="newUser" >
+<button class="p-2 px-5 bg-green-500 disabled:bg-slate-400" :disabled="newUser.length == 0">add</button>
 </template>
