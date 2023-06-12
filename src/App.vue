819de29cacc7649ec3de23c2cf2c6e04a637a6ef
lparamedicl <kondaurovr1@gmail.com>
2023-06-12 17:47:38 +0500

    xz

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup>
+import { ref } from 'vue';
+import listinvite from './components/listinvite.vue'
 
+
+const users = ref(['cancer'])
+
+const newUser = ref('');
 </script>
 
 <template>
-    <form @:submit.prevent="addUser">{{ newUser }}</form>
+<listinvite v-bind:users="users"></listinvite>
+<form action="" @:submit.prevent="addUser">
+{{ newUser }}
+<input type="text" name="user_name" v-model="newUser">
+<button class="py-5 px-5 bg-green-600 disabled:bg-slate-400" :disabled="newUser.length == 0">add User</button>
+
+
+</form>
+</template>
+
+
+
+<form @:submit.prevent="addUser">{{ newUser }}</form>
 <input type="text" name="username" v-model="newUser" >
 <button class="p-2 px-5 bg-green-500 disabled:bg-slate-400" :disabled="newUser.length == 0">add</button>
-</template>
---
+++ src/components/listinvite.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps ({
+    users: {
+        type: Array,
+        default: [],
+    }
+});
+</script>
+
+<template>
+<ul>
+    <li v-for="(user, index) in users">
+        {{ index + 1 }}. {{ user }}
+    </li>
+</ul>
+</template>
