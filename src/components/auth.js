204e2436512632ace35cf27bdc8527e39d8d7f47
lparamedicl <kondaurovr1@gmail.com>
2023-06-16 19:36:42 +0500

    composables creating

---
+++ src/components/auth.js
@@ -0,0 +1,3 @@
+const user = ref(null)
+
+const users = 
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+<template>
+    <form action="" @submit.prevent="onLogin">
+    <div>
+        <label for="" >
+            email
+        <input type="email" v-model="email">
+        </label>
+    </div>
+<div>
+    <label for="">password
+        <input type="password" v-model="password">
+    </label>
+</div>
+<button>login xz</button>
+</form>
+
+</template>
